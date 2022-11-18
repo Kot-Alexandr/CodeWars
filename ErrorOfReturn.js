40b1cc32d585a4e57b17d666cc9532d23347c1a6
Kot-Alexandr <voollkot@gmail.com>
2022-11-18 15:46:11 +0300

    add task

---
+++ ErrorOfReturn.js
@@ -0,0 +1,18 @@
+function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return results;
+  }
+  or
+  function mystery() {
+    var results =
+      {sanity: 'Hello'};
+    return ( 
+    results);
+  }
+//   function mystery() {
+//     var results =
+//       {sanity: 'Hello'};
+//     return 
+//     results;
+//   }
