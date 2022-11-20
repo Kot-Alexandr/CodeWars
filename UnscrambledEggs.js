9539e4f8dda028257603a0b35ed5ef350eb667a9
Kot-Alexandr <voollkot@gmail.com>
2022-11-20 14:53:42 +0300

    Add Task

---
+++ UnscrambledEggs.js
@@ -0,0 +1,5 @@
+function unscrambleEggs(word){
+   return word.replace(/egg/g, "");
+  }
+//   The string given to your function has had an "egg" inserted directly after each consonant.
+//    You need to return the string before it became eggcoded.
