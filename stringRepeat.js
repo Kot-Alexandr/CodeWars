069b23a019c16ce9d3ab627c8d253ae6d6830ced
Kot-Alexandr <voollkot@gmail.com>
2023-01-07 17:20:17 +0300

    add task

---
+++ stringRepeat.js
@@ -0,0 +1,10 @@
+function repeatStr (n, s) {
+    let ressult="";
+     for (i=1;i<=n;i++) {ressult=ressult+s}
+     return ressult
+  }
+
+
+
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
