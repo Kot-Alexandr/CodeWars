502f34eaa97aea6349f164670d307f6bd2eb2401
Kot-Alexandr <voollkot@gmail.com>
2023-02-01 00:13:32 +0300

    add task

---
+++ unfinishedLoop-BugFixing#1.js
@@ -0,0 +1,15 @@
+const createArray = n => Array(...Array(n)).map((v, i) => i + 1);
+
+
+// function createArray(number){
+//     var newArray = [];
+    
+//     for(var counter = 1; counter <= number;){
+//       newArray.push(counter);
+//     }
+    
+//     return newArray;
+//   }
+
+//   Unfinished Loop - Bug Fixing #1
+//   Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
