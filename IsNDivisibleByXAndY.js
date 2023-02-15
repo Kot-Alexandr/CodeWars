bf41d00d8af67ee41e8a6e5755b09716f285d156
Kot-Alexandr <voollkot@gmail.com>
2023-02-15 17:15:44 +0300

    add task

--- IsNDivisibleByXAndY.js
+++ IsNDivisibleByXAndY.js
@@ -2,4 +2,4 @@ function isDivisible(n, x, y) {
     return n%x==0 && n%y==0
 }
 
-// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers.
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers. 
